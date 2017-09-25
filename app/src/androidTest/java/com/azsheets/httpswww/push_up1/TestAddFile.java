1c5c994504ba924cdb02f8577e39a5011df70a8b
marcomarazzo <30701788+marcomarazzo@users.noreply.github.com>
2017-09-25 15:55:27 +0200

    Add files via upload

---
+++ app/src/androidTest/java/com/azsheets/httpswww/push_up1/TestAddFile.java
@@ -0,0 +1,8 @@
+package com.azsheets.httpswww.push_up1;
+
+/**
+ * Created by Marco Marazzo on 25.09.2017.
+ */
+
+public class TestAddFile {
+}
---
+++ app/src/androidTest/java/com/azsheets/httpswww/push_up1/TestAddFile_B.java
@@ -0,0 +1,9 @@
+package com.azsheets.httpswww.push_up1;
+
+/**
+ * Created by Marco Marazzo on 25.09.2017.
+ */
+
+public class TestAddFile_B {
+    // comment
+}
